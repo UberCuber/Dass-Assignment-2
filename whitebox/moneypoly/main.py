72e742555dca2fb298e8d9b82bb15383b73c1740
Krrish <guptakrrish2006@gmail.com>
2026-03-23 12:37:05 +0530

    Iteration 10: Added module and function docstrings in main.py

--- whitebox/moneypoly/main.py
+++ whitebox/moneypoly/main.py
@@ -1,7 +1,12 @@
+""" 
+main.py defines the main function and the get player names function. 
+"""
+
 from moneypoly.game import Game
 
 
 def get_player_names():
+    """Inputs player names"""
     print("Enter player names separated by commas (minimum 2 players):")
     raw = input("> ").strip()
     names = [n.strip() for n in raw.split(",") if n.strip()]
@@ -9,6 +14,7 @@ def get_player_names():
 
 
 def main():
+    """The main function where the game starts."""
     names = get_player_names()
     try:
         game = Game(names)
